8eb59f52d9cbf78b5925be38a3f18f4125a66d1d
Arnas Savickas <arnas.savi@gmail.com>
2020-12-17 23:35:36 +0200

    finished part4

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,18 +1,66 @@
 const blogsRouter = require('express').Router();
+const { response } = require('../app');
 const Blog = require('../models/blog');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+require('dotenv');
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get('/', async (request, response) => {
+  const blogsResponse = await Blog.find({}).populate('user', { blogs: 0 });
+  response.json(blogsResponse);
 });
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body);
+blogsRouter.get('/:id', async (request, response) => {
+  const blogsResponse = await Blog.find({ _id: request.params.id });
+  if (blogsResponse) {
+    response.json(blogsResponse);
+  } else {
+    response.status(404).end();
+  }
+});
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body;
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const user = await User.findById(body.userId);
+
+  const blog = new Blog(body);
+
+  blog.user = user._id;
+
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+  response.json(savedBlog);
+});
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const blogToDelete = await Blog.findById(request.params.id);
+
+  if (blogToDelete.user.toString() === decodedToken.id) {
+    await Blog.deleteOne({ _id: request.params.id });
+    response.status(204).end();
+  } else {
+    response
+      .status(401)
+      .json({ error: 'user is not the creator of this blog' });
+  }
+});
+
+blogsRouter.put('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  blog.likes = request.body.likes;
+  blog.save();
+  response.status(200).end();
 });
 
 module.exports = blogsRouter;
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -3,10 +3,6 @@ const http = require('http');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
 const server = http.createServer(app);
 
 server.listen(config.PORT, () => {
