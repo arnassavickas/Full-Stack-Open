5027098951b87e3ac6b36cde56c9d3ab993de375
Arnas Savickas <33692872+arnassavickas@users.noreply.github.com>
2020-12-19 13:29:56 +0200

    Update App.js

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,24 +1,49 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import Blogs from './components/Blogs';
+import Login from './components/Login';
+import Notification from './components/Notification';
+import blogService from './services/blogs';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null);
+  const [notification, setNotification] = useState({
+    text: null,
+    color: null,
+  });
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    const localUser = window.localStorage.getItem('user');
+    if (localUser) {
+      const userData = JSON.parse(localUser);
+      setUser(userData);
+      blogService.setToken(userData.token);
+    }
+  }, []);
+
+  const showNotification = (text, color, time) => {
+    setNotification({
+      text,
+      color,
+    });
+    setTimeout(() => {
+      setNotification({ text: null, color: null });
+    }, time);
+  };
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      <Notification notification={notification} />
+      {user ? (
+        <Blogs
+          user={user}
+          setUser={setUser}
+          showNotification={showNotification}
+        />
+      ) : (
+        <Login showNotification={showNotification} setUser={setUser} />
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
