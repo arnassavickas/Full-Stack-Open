8bc5f378a4c9412c115a5c023d56b8b1540713c4
Arnas Savickas <arnas.savi@gmail.com>
2020-12-19 13:50:03 +0200

    restored fully part5.6

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,21 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from 'axios';
+const baseUrl = '/api/blogs';
+
+let token = null;
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`;
+};
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
-export default { getAll }
+const addNewBlog = async (blogData) => {
+  const config = { headers: { Authorization: token } };
+  const response = await axios.post(baseUrl, blogData, config);
+  return response.data;
+};
+
+export default { getAll, addNewBlog, setToken };
