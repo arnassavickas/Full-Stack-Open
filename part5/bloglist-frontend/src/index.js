bdc1eb3011634065ca5248e0752fef19d10646ec
Arnas Savickas <33692872+arnassavickas@users.noreply.github.com>
2020-12-19 13:30:28 +0200

    Update index.js

--- part5/bloglist-frontend/src/index.js
+++ part5/bloglist-frontend/src/index.js
@@ -1,5 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import 'react-hot-loader';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader/root';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+export default hot(App);
+
+ReactDOM.render(<App />, document.getElementById('root'));
